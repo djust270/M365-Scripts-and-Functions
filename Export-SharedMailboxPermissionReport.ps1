2a8c1120f098147b7e62f82884cda14104a89ec4
David Just <57944742+djust270@users.noreply.github.com>
2022-10-25 16:50:34 -0400

    Add files via upload

---
+++ Export-SharedMailboxPermissionReport.ps1
@@ -0,0 +1,59 @@
+
+Function Get-SaveFolderLocation
+{
+	[Cmdletbinding()]
+	param(
+		[String]$Description
+	)
+	[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") |	Out-Null
+	$SaveFolderDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+	$SaveFolderDialog.Description = $Description | out-null
+	$SaveFolderDialog.ShowDialog() | out-null
+	return $SaveFolderDialog.SelectedPath
+}
+Write-Host "Select a folder to save this report to:" -ForegroundColor Green -BackgroundColor Black
+$ReportFolder = Get-SaveFolderLocation
+$prereqmodules = @(
+	'ImportExcel'	
+	'ExchangeOnlineManagement'
+)
+Write-Host "Checking Available Modules"
+$available = Get-Module -ListAvailable | Select-Object -ExpandProperty Name
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+foreach ($module in $prereqmodules)
+{
+	if ($module -notin $available)
+	{
+		"{0} module not detected, attempting to install..." -f $module			
+		if (-not $isadmin)
+		{
+			install-module $module -scope CurrentUser			
+		}
+		else
+		{
+			Install-Module $module			
+		}
+	}
+}
+Write-Host "Connecting to Exhange Online..."
+Connect-ExchangeOnline -ShowBanner:$false
+$DefaultDomain = (Get-AcceptedDomain | Where-Object {$_.default}).Name
+$SharedMailboxes = Get-Mailbox -Filter { isShared -eq 'true' } -Resultsize Unlimited
+$i = 1
+$PermissionReportArray = foreach ($box in $SharedMailboxes){
+    Write-Progress -Activity "Working on Shared Mailbox Permission Report" -Status "Working on $($box.Identity)" -PercentComplete (($i / $SharedMailboxes.Count) * 100)
+    $Permissions = Get-MailboxPermission -Identity $box.Guid.Guid
+    $Permissions | ForEach-Object {
+        [PSCustomObject]@{
+            Identity = $_.identity
+            PrimarySMTPAddress = $box.PrimarySMTPAddress
+            User = $_.User
+            AccessRights = $($_.AccessRights -join ',')
+        }        
+    }
+    $i++
+}
+$PermissionReportArray | Export-Excel -Path "$ReportFolder\$DefaultDomain-SharedMailboxPermissionReport.xlsx" -WorksheetName "SharedMailboxReport" -TableName "SharedPermissions" -AutoSize
+Write-Host "Report saved to "  $("$ReportFolder\$DefaultDomain-SharedMailboxPermissionReport.xlsx")
+& "$ReportFolder\$DefaultDomain-SharedMailboxPermissionReport.xlsx"
+Disconnect-ExchangeOnline
